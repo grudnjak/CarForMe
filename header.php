312028deca89382dfa9214a6f187636b2bf63722
Valentin Grudnik <wallicar99@gmail.com>
2018-09-08 13:05:44 +0200

    CSS

---
+++ header.php
@@ -0,0 +1,69 @@
+<?php
+    include_once './session.php';
+?>
+<!DOCTYPE HTML>
+<!--
+	Ion by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>CAR4ME</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body id="top">
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="index.php">CAR4ME</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="apartmaji.php">Avtomobili</a></li>
+                                                    <?php
+                                        
+                                        if (isset($_SESSION['user_id'])) {
+                                            echo '  <li><a href="dodajanje_apartmaji.php">Dodaj apartma</a></li>';
+                                        }
+                                        else {
+                                            
+                                            echo ' ';
+                                        }
+                                        if (isset($_SESSION['admin'])) {
+                                            echo '<li><a href="adminpanel.php">Admin panel</a></li>';
+                                        }
+                                    ?>
+                                              
+						<li><a href="info.php">O nas</a></li>
+                                                <li></li>
+                                                  <?php
+                                        
+                                        
+                                        if (isset($_SESSION['user_id'])) {
+                                            echo '<a href="logout.php" class="button alt">Log out</a>';
+                                        }
+                                        else {
+                                            echo ' <a href="login.php" class="button special ">Log in</a> ';
+                                            echo ' <a href="registration.php" class="button alt">Register</a> ';
+                                        }
+                                    ?>
+                                                
+                                      <!--       echo '<li><a href="registration.php" class="button special">Sign Up</a></li>';
+                                                
+                                                ?>-->
+					</ul>
+				</nav>
+			</header>
--- session.php
+++ session.php
@@ -2,10 +2,10 @@
     session_start();
     
     
-$allowed = ['/projekt_apartmaji/login.php','/projekt_apartmaji/login_check.php',
-     '/projekt_apartmaji/registration.php', '/projekt_apartmaji/user_insert.php',
-      '/projekt_apartmaji/index.php','/projekt_apartmaji/apartmaji.php',
-    '/projekt_apartmaji/info.php'];
+$allowed = ['/CarForMe/login.php','/CarForMe/login_check.php',
+     '/CarForMe/registration.php', '/CarForMe/user_insert.php',
+      '/CarForMe/index.php','/CarForMe/apartmaji.php',
+    '/CarForMe/info.php'];
     
     
  if (!isset($_SESSION['user_id']) && 
