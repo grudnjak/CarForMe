4044a1d686af6fffe81bead2334363f589f2c6ef
Valentin Grudnik <wallicar99@gmail.com>
2018-09-23 18:04:32 +0200

    Delete/edit

--- avtomobili.php
+++ avtomobili.php
@@ -7,7 +7,7 @@ include_once './header.php';
 
 include_once './database.php';
 
-$query = "SELECT a.id, a.ime ,a.opis,k.ime,m.ime,z.ime FROM avtomobili a INNER JOIN kraji k ON k.id = a.kraj_id INNER JOIN modeli m ON m.id=a.model_id INNER JOIN znamke z ON z.id=m.znamka_id";
+$query = "SELECT a.id as aid, a.ime  as aime ,a.opis as aopis,k.ime as kime ,m.ime as mime,z.ime as zime, o.id as oid FROM avtomobili a INNER JOIN kraji k ON k.id = a.kraj_id INNER JOIN modeli m ON m.id=a.model_id INNER JOIN znamke z ON z.id=m.znamka_id INNER JOIN osebe o ON o.id=a.oseba_id";
 $stmt = $pdo->prepare($query);
 $stmt->execute();
 
@@ -15,12 +15,12 @@ $stmt->execute();
 
 echo "<table>";
 echo "<tr><td>Ime</td><td>Opis</td><td>Kraj</td><td>Model </td> <td>Znamka</td><td></td> <td>  </td> </tr>";
-while ($row = $stmt->fetch(true)) {
+while ($row = $stmt->fetch()) {
     
-    echo " <tr><td><a href=\"avtomobil.php?id=$row[0]\">" . $row['1'] . "</a></td><td>" . $row['2'] . "</td><td>" . $row['3'] . "</td><td>" . $row['4'] . "</td><td>" . $row['5'] . "</td>";
+    echo " <tr><td><a href=\"avtomobil.php?id=$row[aid]\">" . $row['aime'] . "</a></td><td>" . $row['aopis'] . "</td><td>" . $row['kime'] . "</td><td>" . $row['mime'] . "</td><td>" . $row['zime'] . "</td>";
     if (isset($_SESSION['user_id'])) {
-        if ($_SESSION['user_id'] == $row[5]) {
-            echo "<td><a href=\"avtomobil_edit.php?id=$row[0]\">Uredi</a></td><td><a href=\"avtomobil_delete.php?id=$row[0]\">Izbrisi</a></td></tr>";
+           if ($_SESSION['user_id'] == $row['oid']) {
+            echo "<td><a href=\"avtomobili_edit.php?id=$row[aid]\">Uredi</a></td><td><a href=\"avtomobili_delete.php?id=$row[aid]\">Izbrisi</a></td></tr>";
         } else {
 
             echo "<td></td><td></td>";
---
+++ avtomobili_edit.php
@@ -0,0 +1,41 @@
+<?php
+include_once './header.php';
+include_once './database.php';
+
+$idapp = isset($_GET['id']) ? $_GET['id'] : '';
+
+
+$query4 = "SELECT a.id as aid,a.ime as aime,a.opis as aopis,a.naslov as anaslov,m.ime as mime, a.model_id as model FROM avtomobili a INNER JOIN kraji k ON k.id=a.kraj_id INNER JOIN modeli m ON m.id=a.model_id WHERE a.id=? ";
+$stmt = $pdo->prepare($query4);
+
+$stmt->execute([$idapp]);
+$row = $stmt->fetch();
+$model= $row['model'];
+$query = "SELECT m.ime FROM modeli m INNER JOIN avtomobili a ON m.id=a.model_id WHERE a.model_id=? LIMIT 1";
+$stmt1 = $pdo->prepare($query);
+$stmt1->execute([$model]);
+$options1 = "";
+while ($row1 = $stmt1->fetch()) {
+
+    $options1 = $options1 . "<option>$row1[ime]</option>";
+}
+?>
+<div class="box">
+    <h1>Spremeni podatke o avtomobilu</h1>
+
+    <form action="car_update.php" method="post">
+        <input type="hidden" name="id" value="<?php echo $row['aid']; ?>" />
+        <label>Ime</label>
+        <input type="text" name="ime" value="<?php echo $row['aime']; ?>" required="required" />
+        <label>Opis</label>
+        <input type="text" name="opis" value="<?php echo $row['aopis']; ?>" required="required" />
+        <label>Naslov</label>
+        <input type="text" name="naslov" value="<?php echo $row['anaslov']; ?>" required="required" />
+        <label>Model</label>
+        <select name="app_model">
+            <?php echo $options1; ?>
+        </select>
+        <br />
+        <input type="submit" value="Shrani" />
+    </form>
+</div>
